378ff61333fb1de5f9cc612f32e8bbb2e9c31a6a
hdyson <hdyson@gmail.com>
2017-12-10 17:56:00 +0000

    Updated python config to use elpy

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 ## Customisations for emacs for python development.
 
+Corresponding ```.emacs``` file is just:
+
+```
+(load "~/.emacs.d/init.el" nil t)
+```
+
+Using a separate init file isolates changes via the custom interface to the init.el file.
+
 Includes required emacs packages, requires these python packages to be install too (via pip or similar).
 
 For [flycheck](http://www.flycheck.org/manual/0.25.1/index.html):
@@ -11,3 +19,9 @@ For [Jedi.el](http://tkf.github.io/emacs-jedi/latest/):
 1. virtualenv
 2. epc
 3. jedi
+
+Reminder for activating/deactivating a venv/conda env:
+```
+(pyvenv-activate (expand-file-name "~/path/to/venv"))
+(pyvenv-deactivate)
+```
--- init.el
+++ init.el
@@ -1,3 +1,27 @@
+(require 'package) ;; You might already have this line
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar myPackages
+  '(elpy
+    flycheck
+    py-autopep8
+    ))
+
+(mapc #'(lambda (package)
+    (unless (package-installed-p package)
+      (package-install package)))
+      myPackages)
+
+
 (if (file-readable-p "~/.emacs.d/config/basic.el")
        (load "~/.emacs.d/config/basic.el" nil t))
 (if (file-readable-p "~/.emacs.d/config/keys.el")
