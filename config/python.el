f8b0228848b218fc29677295755826f52568299b
hdyson <hdyson@gmail.com>
2018-02-13 22:58:56 +0000

    Updated ipython call

--- config/python.el
+++ config/python.el
@@ -6,5 +6,10 @@
   (add-hook 'elpy-mode-hook 'flycheck-mode))
 
 ;; enable autopep8 formatting on save
-(require 'py-autopep8)
-(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+;; (require 'py-autopep8)
+;; (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+
+;; Use ipython for shell
+;; See: https://elpy.readthedocs.io/en/latest/ide.html#interpreter-setup
+(setq python-shell-interpreter "ipython"
+      python-shell-interpreter-args "-i --simple-prompt")
