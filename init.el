1f8255ee7a2d98f6a1f53126de6b695b552803c8
Harold Dyson <hdyson@gmail.com>
2019-04-28 12:50:59 +0100

    Clearing out branch to match state described on blog

--- init.el
+++ init.el
@@ -1,39 +1,24 @@
-(defvar myConfigFiles
-  '("~/.emacs.d/config/packages.el"
-    "~/.emacs.d/config/basic.el"
-    "~/.emacs.d/config/keys.el"
-    "~/.emacs.d/config/python.el"
-    "~/.emacs.d/config/functions.el"
-    ))
+(add-to-list 'package-archives
+	     '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(package-initialize)
 
-(mapc #'(lambda (config)
-	  (if (file-readable-p config) (load config nil t)))
-      myConfigFiles)
+(elpy-enable)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (tango-dark)))
- '(eshell-buffer-maximum-lines 10240)
- '(eshell-cmpl-compare-entry-function (quote string-lessp))
- '(eshell-cmpl-cycle-completions nil)
- '(eshell-history-size 100000)
- '(eshell-modules-list
-   (quote
-    (eshell-alias eshell-banner eshell-basic eshell-cmpl eshell-dirs eshell-glob eshell-hist eshell-ls eshell-pred eshell-prompt eshell-script eshell-smart eshell-term eshell-unix eshell-xtra)))
- '(mouse-avoidance-mode nil nil (avoid))
- '(package-selected-packages
-   (quote
-    (which-key multiple-cursors markdown-mode magit iedit py-autopep8 flycheck elpy)))
- '(revert-without-query (quote (".*\\.err" ".*\\.out")))
- '(switch-to-buffer-in-dedicated-window (quote prompt))
- '(tab-always-indent (quote complete))
- '(tab-stop-list (quote (4 8))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(defun create-blogpost (x)
+  "Create a new blogpost."
+  (interactive "sPost title: ")
+  (let ((file-date (shell-command-to-string "echo -n $(date +%Y-%m-%d)"))
+	(file-title (s-replace " " "-" x))
+	(post-date (shell-command-to-string (concat "echo -n $(date " (shell-quote-argument "+%Y-%m-%d %H:%M:%S") ")")))
+	(post-title x)
+	filename filepath)
+    (setf filename (concat file-date "-" file-title ".md"))
+    (setf filepath (concat "/path/to/local/_posts/directory" filename))
+    (write-region (concat "---
+layout: default
+title:  " post-title "
+date:   " post-date "
+categories: emacs
+---
+") 0 filepath t)
+    (find-file filepath)))
