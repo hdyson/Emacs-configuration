8250de3751471567a0ad8f1204a0434d3dfcde5e
Harold Dyson <hdyson@gmail.com>
2019-11-12 22:02:27 +0000

    Org-babel-ified init file

--- init.el
+++ init.el
@@ -1,20 +1,39 @@
-(defvar myConfigFiles
-  '("~/.emacs.d/config/packages.el"
-    "~/.emacs.d/config/basic.el"
-    "~/.emacs.d/config/keys.el"
-    "~/.emacs.d/config/python.el"
-    "~/.emacs.d/config/functions.el"
-    ))
+;;; init.el --- Where all the magic begins
+;;
+;; This file loads Org-mode and then loads the rest of our Emacs initialization from Emacs lisp
+;; embedded in literate Org-mode files.
 
-(mapc #'(lambda (config)
-	  (if (file-readable-p config) (load config nil t)))
-      myConfigFiles)
+;; Load up Org Mode and (now included) Org Babel for elisp embedded in Org Mode files
+(setq dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
+
+(let* ((org-dir (expand-file-name
+                 "lisp" (expand-file-name
+                         "org" (expand-file-name
+                                "src" dotfiles-dir))))
+       (org-contrib-dir (expand-file-name
+                         "lisp" (expand-file-name
+                                 "contrib" (expand-file-name
+                                            ".." org-dir))))
+       (load-path (append (list org-dir org-contrib-dir)
+                          (or load-path nil))))
+  ;; load up Org-mode and Org-babel
+  (require 'org-install)
+  (require 'ob-tangle))
+
+;; load up all literate org-mode files in this directory
+(mapc #'org-babel-load-file (directory-files dotfiles-dir t "\\.org$"))
+
+;;; init.el ends here
+
+;;; Below here is where the customise interface sets variables -
+;;; usually settings I'm experimenting with but haven't committed to.
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(backup-directory-alist (quote (("." . "~/.emacs.backups"))))
  '(custom-enabled-themes (quote (tango-dark)))
  '(eshell-buffer-maximum-lines 10240)
  '(eshell-cmpl-compare-entry-function (quote string-lessp))
