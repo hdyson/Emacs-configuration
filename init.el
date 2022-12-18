ce189d37ddb62c99fcf44cdacf819815bec363ec
Harold Dyson <hdyson@me.com>
2022-12-18 19:45:41 +0000

    Tidied up the formatting

--- init.el
+++ init.el
@@ -34,21 +34,24 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(backup-directory-alist (quote (("." . "~/.emacs.backups"))))
- '(custom-enabled-themes (quote (tango-dark)))
+ '(backup-directory-alist '(("." . "~/.emacs.backups")))
+ '(custom-enabled-themes '(tango-dark))
  '(elpy-rpc-python-command "python3")
+ '(elpy-test-pytest-runner-command
+   '("/Users/hdyson/Repos/Git/AdventOfCode2022/venv/bin/python" "-m" "pytest"))
  '(eshell-buffer-maximum-lines 10240)
- '(eshell-cmpl-compare-entry-function (quote string-lessp))
+ '(eshell-cmpl-compare-entry-function 'string-lessp)
  '(eshell-cmpl-cycle-completions nil)
  '(eshell-history-size 100000)
  '(eshell-modules-list
-   (quote
-    (eshell-alias eshell-banner eshell-basic eshell-cmpl eshell-dirs eshell-glob eshell-hist eshell-ls eshell-pred eshell-prompt eshell-script eshell-smart eshell-term eshell-unix eshell-xtra)))
+   '(eshell-alias eshell-banner eshell-basic eshell-cmpl eshell-dirs eshell-glob eshell-hist eshell-ls eshell-pred eshell-prompt eshell-script eshell-smart eshell-term eshell-unix eshell-xtra))
  '(mouse-avoidance-mode nil nil (avoid))
- '(revert-without-query (quote (".*\\.err" ".*\\.out")))
- '(switch-to-buffer-in-dedicated-window (quote prompt))
- '(tab-always-indent (quote complete))
- '(tab-stop-list (quote (4 8))))
+ '(package-selected-packages
+   '(which-key use-package swiper py-autopep8 projectile multiple-cursors markdown-mode magit iedit flycheck flx-ido exec-path-from-shell elpy doom-modeline all-the-icons))
+ '(revert-without-query '(".*\\.err" ".*\\.out"))
+ '(switch-to-buffer-in-dedicated-window 'prompt)
+ '(tab-always-indent 'complete)
+ '(tab-stop-list '(4 8)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
