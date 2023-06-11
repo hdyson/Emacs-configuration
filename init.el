c69aa8004c9157a8e586a5ec0f59498ec9f0d4ad
Harold Dyson <hdyson@me.com>
2023-06-11 19:10:48 +0100

    Cleaned out redundant settings from init.el

--- init.el
+++ init.el
@@ -25,36 +25,3 @@
 (mapc #'org-babel-load-file (directory-files dotfiles-dir t "\\.org$"))
 
 ;;; init.el ends here
-
-;;; Below here is where the customise interface sets variables -
-;;; usually settings I'm experimenting with but haven't committed to.
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(backup-directory-alist '(("." . "~/.emacs.backups")))
- '(custom-enabled-themes '(tango-dark))
- '(elpy-rpc-python-command "python3")
- '(elpy-test-pytest-runner-command
-   '("/Users/hdyson/Repos/Git/AdventOfCode2022/venv/bin/python" "-m" "pytest"))
- '(eshell-buffer-maximum-lines 10240)
- '(eshell-cmpl-compare-entry-function 'string-lessp)
- '(eshell-cmpl-cycle-completions nil)
- '(eshell-history-size 100000)
- '(eshell-modules-list
-   '(eshell-alias eshell-banner eshell-basic eshell-cmpl eshell-dirs eshell-glob eshell-hist eshell-ls eshell-pred eshell-prompt eshell-script eshell-smart eshell-term eshell-unix eshell-xtra))
- '(mouse-avoidance-mode nil nil (avoid))
- '(package-selected-packages
-   '(lingva mastodon which-key use-package swiper py-autopep8 projectile multiple-cursors markdown-mode magit iedit flycheck flx-ido exec-path-from-shell elpy doom-modeline all-the-icons))
- '(revert-without-query '(".*\\.err" ".*\\.out"))
- '(switch-to-buffer-in-dedicated-window 'prompt)
- '(tab-always-indent 'complete)
- '(tab-stop-list '(4 8)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(minimap-font-face ((t (:height 15 :family "DejaVu Sans Mono")))))
